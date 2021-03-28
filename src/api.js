3121e22f0804d95274780b025102c253527fd1f4
BenBJD <nimijneb.bd@gmail.com>
2021-03-28 20:13:24 +0100

    add other api requests

--- src/api.js
+++ src/api.js
@@ -2,8 +2,54 @@ export const search = (term, indexer, category) => {
     let params = {
         q: term,
         category: category,
+        indexer: indexer
     }
-    fetch(`192.168.1.20:5080/search/${indexer}` + new URLSearchParams(params))
+    fetch(`${window.location.hostname}:${window.location.port}/search` + new URLSearchParams(params))
+        .then(function (response) {
+            if (response.status !== 200) {
+                console.log("Status code:" + response.status)
+                return response.status
+            }
+            return response.json()
+        })
+        .catch(function (err) {
+            console.log("Fetch Error", err)
+        })
+}
+
+export const indexers = () => {
+    fetch(`${window.location.hostname}:${window.location.port}/indexers`)
+        .then(function (response) {
+            if (response.status !== 200) {
+                console.log("Status code:" + response.status)
+                return response.status
+            }
+            return response.json()
+        })
+        .catch(function (err) {
+            console.log("Fetch Error", err)
+        })
+}
+
+export const recents = () => {
+    fetch(`${window.location.hostname}:${window.location.port}/recents`)
+        .then(function (response) {
+            if (response.status !== 200) {
+                console.log("Status code:" + response.status)
+                return response.status
+            }
+            return response.json()
+        })
+        .catch(function (err) {
+            console.log("Fetch Error", err)
+        })
+}
+
+export const removeRecent = (item) => {
+    let params = {
+        item: item
+    }
+    fetch(`${window.location.hostname}:${window.location.port}/removeRecent` + new URLSearchParams(params))
         .then(function (response) {
             if (response.status !== 200) {
                 console.log("Status code:" + response.status)
